c8a2c0cd3805571650ed56ef66281211c502ea2e
jenil sangani <jenilsangani2000@gmail.com>
2022-12-06 14:40:32 +0530

    add logic

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,13 @@
 const connectToMongo = require('./database');
+const express = require('express')
 connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Jenil!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
